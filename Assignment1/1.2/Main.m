4831fce337c2bbc9c3718ecffcf78d7b018898c2
Adam Tonderski <atonderski@gmail.com>
2017-09-11 16:25:02 +0200

    Create flow for assignment 1.2
    Updated the main method to plot a graph of 20 different simulations
    of order parameters over time.
    
    Created the function SimulateOrderParameter that perform one simulation
    of order parameter over time

--- Assignment1/1.2/Main.m
+++ Assignment1/1.2/Main.m
@@ -1,24 +1,18 @@
-clear all
-close all
+clearvars, close all
 nNeurons = 200;
-nIndependentBits = 10^5;
-patternNumbers = [1, 20, 40, 60, 80, 100, 120, 140, 160, 180, 200, 220, 240, 260, 280, 300, 320, 340, 360, 380, 400];
-
-patternSpace = linspace(1,400);
-pErrorTheoretical = TheoreticalErrorProbability(patternSpace, nNeurons);
-plot(patternSpace/nNeurons, pErrorTheoretical)
+nPatterns = 5;
+noiseParameter = 2;
+iterations = 20;
+timePerIteration = 10^3;
 
 hold on
-
-% Draw experimental results for comparison
-for iNumPatterns = patternNumbers
-    pError = EstimateOneStepError(iNumPatterns, nNeurons, nIndependentBits);
-    alpha = iNumPatterns/nNeurons; 
-    plot(alpha, pError, 'ro')
+timeSeries = 1:timePerIteration;
+for i = 1:iterations
+    orderParameterSeries = SimulateOrderParameter(nPatterns, nNeurons, noiseParameter, timePerIteration);
+    plot(timeSeries, orderParameterSeries)
     drawnow
 end
 
-title('Graph of one step error probability')
-xlabel('p/N') % x-axis label
-ylabel('P_{Error}') % y-axis label
-legend('theoretical curve','experimental results')
+title('Order parameters over time')
+xlabel('time')
+ylabel('Order parameter')
---
+++ Assignment1/1.2/SimulateOrderParameter.m
@@ -0,0 +1,23 @@
+function orderParameterValues = SimulateOrderParameter(nPatterns, nNeurons, noiseParameter, timeToRun)
+
+    %Initialize new patterns
+    patterns = zeros(nPatterns,nNeurons);
+    for i = 1:nPatterns
+        patterns(i,:) = GenerateRandomPattern(nNeurons);
+    end    
+    weights = CalculateWeights(patterns);
+
+    
+    initialState = patterns(1,:);
+    currentState = initialState;
+    
+    orderParameterValues = zeros(timeToRun,1);
+    t = 1;
+    while t <= timeToRun    
+        currentState = UpdateState(currentState, weights, noiseParameter);
+        orderParameterValues(t) = CalculateOrderParameter(currentState, initialState);
+        t = t + 1;
+    end
+
+end
+
