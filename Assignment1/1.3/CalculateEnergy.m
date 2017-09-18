a4c75f39cd68d3571ded5e87f5210dd7f904736d
mpbejmer <mpbejmer@gmail.com>
2017-09-18 22:09:33 +0200

    Just starting on the third problem

---
+++ Assignment1/1.3/CalculateEnergy.m
@@ -0,0 +1,10 @@
+function [ energy ] = EnergyFunction( zeta, output )
+%CalculateEnergy: calculates the error of the output(output) compared to the
+%target(zeta)
+    energy = 0;
+    for i=1:length(output)
+        energy = energy + 1/2*(zeta(i)-output(i))^2;
+    end
+
+end
+
