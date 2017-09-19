cf66aabb4a0bb5e59b1165a57c4df47c9b7ad398
mpbejmer <mpbejmer@gmail.com>
2017-09-19 08:57:41 +0200

    Created InitializeWeights function

---
+++ Assignment1/1.3/InitializeWeights.m
@@ -0,0 +1,14 @@
+function [ weights ] = InitializeWeights( interval, dimensions )
+%InitializeWeights: Creates a matrix with weights with
+%dimensions(dimensions=[M, N]). Values for weights are chosen randomly from
+%a uniform distribution on the interval (interval = [-x, +x]).
+
+    weights = zeros(interval);
+    for i=1:dimensions(1)
+        for j=1:dimensions(2)
+            weights(i,j) = interval(1)+(interval(2)-interval(1))*rand;
+        end
+    end
+
+end
+
